95fdef9b7bf1417a03919f14bdc8b606b762e60a
Rogério Chimionato <chimionato.rogerio@gmail.com>
2024-11-09 19:48:00 -0300

    init commit

---
+++ src/test/java/com/chimionato/Produdoro/ProdudoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chimionato.Produdoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdudoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
